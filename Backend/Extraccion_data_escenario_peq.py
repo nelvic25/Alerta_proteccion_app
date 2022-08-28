89f75a389ef1b006f1f895dec571a4062b12f2c5
Nelson Devops <nelsonvicvera@gmail.com>
2022-08-28 18:49:31 -0500

    Separacion de codigo
    Se elimino/separo/modifico el codigo para terminar de programar como seria la implementacion de un escenario grande y uno pequeno. Ahora solo se tiene uno pequeno

--- Backend/Extraccion_data_escenario_peq.py
+++ Backend/Extraccion_data_escenario_peq.py
@@ -1,3 +1,4 @@
+from heapq import merge
 from turtle import title
 import netmiko as nk
 from netmiko import ConnectHandler, NetmikoTimeoutException
@@ -41,38 +42,21 @@ try:
     connection1 = nk.ConnectHandler(**Ap_credentials)
     Ap_data = connection1.send_command("iwinfo wl0 info")
     Ap_data_splitline = Ap_data.splitlines()[1:2:1]
+    Ap_data_splitline2 = Ap_data.splitlines()[:1:1] #PARA SACAR EL NOMBRE DEL ACCESS POINT
     Ap_data_list=Ap_data_splitline[0].split()
+    Ap_data_list2=Ap_data_splitline2[0].split()
     Ap_macaddress=Ap_data_list[-1]
+
+    AP_name=Ap_data_list2[-1]
     #Se obtiene la MAC Address del router de la victima
 
     clientes_Conectados=[]
+
     Ap_data = connection1.send_command("iwinfo wl0 assoc")
     Ap_data_splitline = Ap_data.splitlines()
     for clientes in Ap_data_splitline:
         clientes_Conectados.append(clientes.split()[0])
     #Se obtiene las MAC Address de los clientes conectados al router
-
-
-    # doc_ref1 = db.collection('mac_attackers').document(Ap_macaddress)
-    # doc = doc_ref1.get()
-    # doc_coleccion=doc.to_dict().copy()#check if remove copy
-    # n=1
-    # for conectado in clientes_Conectados:
-    #     for clave in doc_coleccion :
-    #         atacante=doc_coleccion.get(clave)
-    #         if conectado==atacante:
-    #             nombreDocumento = "Alerta" + n
-    #             now = datetime.now()
-    #             timestamp = datetime.timestamp(now)
-    #             datos = {
-    #                 "MAC Agresor": atacante,
-    #                 "MAC Victima":conectado,
-    #                 "Fecha": timestamp
-    #             }
-    #             print(datos)
-    #             doc_ref2 = db.collection('alerts').document(nombreDocumento).set(datos)
-    #             n=n+1
-    #             #Almacena en la base de datos en la conexion con el trigger
 
     doc_ref1 = db.collection('mac_attackers').document(Ap_macaddress)
     doc = doc_ref1.get()
@@ -91,7 +75,7 @@ try:
                     "fecha": timestamp
                 }
                 print(datos)
-                doc_ref2 = db.collection('alerts').document(nombreDocumento).set(datos)
+                #doc_ref2 = db.collection('alerts').document(nombreDocumento).set(datos,merge=True)
                 n=n+1
                 #Almacena en la base de datos de alertas 
 
@@ -99,20 +83,23 @@ try:
                 
                 message = messaging.Message(
                     notification = messaging.Notification(
-                        title='ALERTA'
+                        title='ALERTA',
                         body='PRUEBAs'
                     )
                     data={
                         'time': timestamp,
                     },
-                    token=registration_token,
+                    token=registration_token
                 )
                 response = messaging.send(message)
-                print('Successfully sent message:', response)
-
+                print('Successfully sent message:', response)  
+except Exception as e:
+    print(e)
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+       
                 
-
-
+""" 
         # Modelo Matemático de canal PATH LOSS
         # RSSI=-10nlog(d/d0)+RSSI0 - 15
 
@@ -122,111 +109,50 @@ try:
         d1 = 10**(rssiT1)
         rssiT2 = (rssi0-(valor_rssi[0])-15)/(10*n)
         d2 = 10**(rssiT2)
+        #rssiT3 = (rssi0-(valor_rssi[1])-15)/(10*n)
+        #d2 = 10**(rssiT2)
 
         # Trilateración Mínimos Cuadrados con solo dos Access Points
         # (xi-x)^2 + (yi-y)^2 = d^2
         x1 = 0
         x2 = 0
+        #x3 = 0
         y1 = -7.35
         y2 = 7.35
+        #y3 = 0
+        #COORDENADAS DE LOS AP CONOCIDOS
 
         x, y = var('x y')
+        #x, y , z= var('x y z')
 
         f1 = (x-x1)**2 + (y-y1)**2 - d1**2
         f2 = (x-x2)**2 + (y-y2)**2 - d2**2
+        #f3 = (x-x3)**2 + (y-y3)**2 - d3**2
 
-        sols = solve((f1, f2), (x, y))
+        sols = solve((f1, f2 ), (x, y))
+        #sols = solve((f1, f2 , f3), (x, y ,z))
         print(sols)
 
-        if(("I" in str(sols[1][0])) or ("I" in str(sols[1][0]))):
+        if(("I" in str(sols[1][0])) or ("I" in str(sols[1][1]))):
             cx = str(sols[1][0])[0:-2]
             cx= float(cx)
             cy = float(sols[1][1])
             coorx = round(cx, 2)
-            coory = round(cy, 2)  
+            coory = round(cy, 2)
             print(cx)
         else: 
             cx = float(sols[1][0])
             cy = float(sols[1][1])
+            cz = float(sols[1][2])
             coorx = round(cx, 2)
-            coory = round(cy, 2)        
-        tuplausmacs = (m, username)
-        usmac.append(tuplausmacs)
-        tuplaxy = (coorx, coory)
-        distancia.append(tuplaxy)
+            coory = round(cy, 2)
 
-        datos2 = {
-            "MAC": m,
-            "Username": username,
-            "coorx": coorx,
-            "coory": coory,
-
+        datos2={
+            "AP_name": AP_name,
+            "PosicionX": coorx,
+            "PosicionY": coory,
         }
+        doc_ref2 = db.collection('global_alerts').document(X) #X ES MAC ADDRESS VERIFICAR DE DONDE SALE , COMO FUNCIONA EL SISTEMA
+        doc_ref2.set(datos2,merge=True)
+ """
 
-        doc_ref1 = db.collection('user_position').document(m)
-        doc_ref1.set(datos2)
-        #Se calcula,agrupa la informacion por MAC  y las coordenadas de donde este se encuentre para luego poder almacenar en
-        #la coleccion user_position.
-except Exception as e:
-    print(e)
-now = datetime.now()
-timestamp = datetime.timestamp(now)
- 
-# Distancia euclidiana entre puntos (distancia entre cada usuario)
-historyarr=[]
-dis_obj =[]
-k = 0
-for i in range(len(distancia)):
-    for j in range(0, len(distancia)):
-        cumple = True
-        print("distancia entre " + usernames[i]+" y "+usernames[j])
-        dis = math.dist(distancia[i], distancia[j])
-        dis = round(dis, 2)
-        print(dis)
-        if(dis < 2 and dis!=0):
-            cumple = False
-            history={
-                'date': now,
-                'distance':dis,
-                'cumple': cumple,
-                'mac': macs[j],
-                'username': usernames[j]
-            }
-            doc_ref3 = db.collection('history_distance_prueba').document(macs[i])
-            doc3 = doc_ref3.get()
-            history_N = {
-                'mac' : macs[i],
-                'username':usernames[i],
-                'last_update': now, 
-                'history':[history]
-            }
-            if doc3.exists:
-                newHistoryN = doc3.to_dict().copy()
-                newHistoryN['last_update'] = now
-                newHistoryN['history'].append(history)
-                print(newHistoryN)
-                doc_ref3.set(newHistoryN)
-            else:
-                print(history_N)
-                doc_ref3.set(history_N)
-        obj = {
-            'mac': macs[j],
-            'username': usernames[j],
-            'distancia': dis,
-            'cumple': cumple,
-            'time': timestamp
-
-        }
-        dis_obj.append(obj)
-        if(i == j):
-            dis_obj.pop(j)
-        if(j == len(distancia)-1):
-            print("ENTROOO")
-            doc_ref2 = db.collection('user_position').document(macs[i])
-            doc2 = doc_ref2.get()
-            nearby = {'nearby': dis_obj}
-            if (doc2.exists):
-                doc_ref2.update(nearby)
-            else:
-                doc_ref2.set(nearby)
-            del dis_obj[0:len(distancia)]
